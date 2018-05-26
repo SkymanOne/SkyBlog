4bd8af13614db9b6046f585773fb1a69f2e1d17d
SkymanOne <german.nikolishin@gmail.com>
2018-05-26 20:55:13 +0300

    base menu

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.env = 'development'
 app.debug = True
 app.testing = True
 app.config.from_object('config')
+bootstrap = Bootstrap(app)
 
 from app import routing
