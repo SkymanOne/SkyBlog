5cc36c01b3974b97d53e4341f87d109ea2a8f57a
SkymanOne <german.nikolishin@gmail.com>
2018-10-27 13:39:01 +0100

    update of secret key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     CSRF_ENABLED = True
-    SECRET_KEY = 'ass-of-your-mother'
+    SECRET_KEY = 'nice-and-cute-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
